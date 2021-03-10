65642b0fc96f0458c9c0d9adfb48915520fd00ee
Umang A <umanga.eng@gmail.com>
2021-03-09 23:21:55 -0500

    [part 3]voyage success! created conditional loop to check the status of ships captain. also addeed the ship status docked or not docked.

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const app = Vue.createApp({
     data: function(){
             return {
-                product: 'Enterprise',
-                description: 'Let explore the final frontier!',
+                ship: 'USS - Enterprise NCC-1701',
+                description: "Let's explore the final frontier!",
                 image: './assets/images/uss_enterprise.jpg',
-                link: 'https://www.anovos.com/products/star-trek-discovery-ncc-1701-u-s-s-enterprise-constitution-class-studio-scale-starship-filming-miniature'
+                link: 'https://www.anovos.com/products/star-trek-discovery-ncc-1701-u-s-s-enterprise-constitution-class-studio-scale-starship-filming-miniature',
+                captain_status: 3,
+                captain: 'J T Kirk',
+                first_officer: 'Spock',
+                highest_rank: 'TBA',
+                docked: true
             }
         }
     })
