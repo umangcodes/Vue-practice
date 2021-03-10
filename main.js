de09b71ae0011ff89a1e78933a29389af48ef2ff
Umang A <umanga.eng@gmail.com>
2021-03-10 11:52:44 -0500

    [part 4]voyage success! hover on events added. warp capabilites improved.

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 const app = Vue.createApp({
     data: function(){
             return {
-                ship: 'USS - Enterprise NCC-1701',
+                ships: ['USS - Enterprise NCC-1701','Vengeance'],
                 description: "Let's explore the final frontier!",
                 image: './assets/images/uss_enterprise.jpg',
                 link: 'https://www.anovos.com/products/star-trek-discovery-ncc-1701-u-s-s-enterprise-constitution-class-studio-scale-starship-filming-miniature',
-                captain_status: 1,
+                captain_status: 3,
                 crew:[
-                    {id : '1', name:'J T Kirk', position:'Captain'},
+                    {id : '3', name:'J T Kirk', position:'Captain'},
                     {id : '2', name:'Spock', position:'first_officer'},
-                    {id : '3', name:'TBA', position:'TBA'}
+                    {id : '1', name:'TBA', position:'TBA'}
                 ],
-                // captain: 'J T Kirk',
-                // first_officer: 'Spock',
-                // highest_rank: 'TBA',
-                docked: true
+                ship_status: ['Docked', 'on voyage', 'destroyed'],
+                ship_sts: 3,
+                ship_health: 3,
+                trips: 1
+            }
+        },
+    methods: {
+        warpOut() {
+            this.ship_sts -= this.trips,
+            this.ship_health -= 1,
+            this.captain_status -= 1,
+            this.trips ++
+        },
+        fallBack() {
+            this.ship_sts += 1
+            // this.ship_health += 1
+            // this.captain_status += 1
+        },
+        changeShip(shipImage){
+            if (shipImage == 'USS - Enterprise NCC-1701'){
+                this.image = './assets/images/uss_enterprise.jpg'
+            }else if (shipImage == 'Vengeance'){
+                this.image = "./assets/images/vengeance.png"
             }
         }
-    })
+
+    }
+})
