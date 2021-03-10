b7ee47140987ed0f22c35ebb710cc56c19369670
Umang A <umanga.eng@gmail.com>
2021-03-09 23:40:59 -0500

    [part 4]voyage success! list rendered along with some conditions.

--- main.js
+++ main.js
@@ -5,10 +5,15 @@ const app = Vue.createApp({
                 description: "Let's explore the final frontier!",
                 image: './assets/images/uss_enterprise.jpg',
                 link: 'https://www.anovos.com/products/star-trek-discovery-ncc-1701-u-s-s-enterprise-constitution-class-studio-scale-starship-filming-miniature',
-                captain_status: 3,
-                captain: 'J T Kirk',
-                first_officer: 'Spock',
-                highest_rank: 'TBA',
+                captain_status: 1,
+                crew:[
+                    {id : '1', name:'J T Kirk', position:'Captain'},
+                    {id : '2', name:'Spock', position:'first_officer'},
+                    {id : '3', name:'TBA', position:'TBA'}
+                ],
+                // captain: 'J T Kirk',
+                // first_officer: 'Spock',
+                // highest_rank: 'TBA',
                 docked: true
             }
         }
