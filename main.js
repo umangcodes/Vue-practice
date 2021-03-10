d301b171a2f554058eb8da9cc55da274650bd20c
Umang A <umanga.eng@gmail.com>
2021-03-09 20:40:46 -0500

    [Part-2]voyage success.

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data: function(){
             return {
                 product: 'Enterprise',
-                description: 'Let explore the final frontier!'
+                description: 'Let explore the final frontier!',
+                image: './assets/images/uss_enterprise.jpg',
+                link: 'https://www.anovos.com/products/star-trek-discovery-ncc-1701-u-s-s-enterprise-constitution-class-studio-scale-starship-filming-miniature'
             }
         }
     })
