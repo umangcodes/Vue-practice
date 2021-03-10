a043d635039759ed87965bfb59b40567bb6254cd
Umang A <umanga.eng@gmail.com>
2021-03-09 20:03:52 -0500

    app object created using vue3

---
+++ main.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data: function(){
+            return {
+                product: 'Enterprise',
+                description: 'Let explore the final frontier!'
+            }
+        }
+    })
